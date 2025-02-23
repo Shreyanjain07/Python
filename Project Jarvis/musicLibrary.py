addf0a186efbbe018eb6061ff05f15c4cf9db2d0
Shreyanjain07 <152770118+Shreyanjain07@users.noreply.github.com>
2025-02-23 10:31:40 +0530

    Create musicLibrary.py
    A music library for jarvis to fetch and play songs from. Linked to youtube.

---
+++ Project Jarvis/musicLibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "stealth": "https://www.youtube.com/watch?v=U47Tr9BB_wE",
+    "flute": "https://www.youtube.com/watch?v=5O118dW7zbc",
+    "firefly": "https://www.youtube.com/watch?v=x_OwcYTNbHs",
+    "adventures": "https://www.youtube.com/watch?v=MkNeIUgNPQ8&list=RDQMauACOy4buYU&start_radio=1"
+}
